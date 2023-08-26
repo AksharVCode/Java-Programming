ea2754cf57ebcc78a245e347623217f33efb97c0
AksharVcode <AksharVCode>
2023-08-26 17:47:05 +0530

    Java programs

---
+++ Operations.java
@@ -0,0 +1,16 @@
+public class Operations {
+    
+    public static void main(String[] args) {
+        int a,b,sum,sub,mul,div;
+        a=10;
+        b=20;
+        sum=a+b;
+        sub=a-b;
+        mul=a*b;
+        div=a/b;
+        System.out.println("Addition is \t"+sum);
+        System.out.println("substraction is \t"+sub);
+        System.out.println("multiplication is \t"+mul);
+        System.out.println("division is \t"+div);
+    }
+}
